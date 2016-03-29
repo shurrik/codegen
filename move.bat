5c925baeea6b9700d147f888764d1fdc2d367a58
shurrik <darkfores123@gmail.com>
2016-03-29 17:18:04 +0800

    '03291718'

--- move.bat
+++ move.bat
@@ -1,7 +1,8 @@
-xcopy .\output\model	D:\workspace\crvci\crvci-domain\src\main\java\com\crv\ci\model /e
-xcopy .\output\dao	D:\workspace\crvci\crvci-biz\src\main\java\com\crv\ci\dao /e
-xcopy .\output\service	D:\workspace\crvci\crvci-biz\src\main\java\com\crv\ci\service /e
-xcopy .\output\mybatis	D:\workspace\crvci\crvci-biz\src\main\resources\mybatis /e
-xcopy .\output\action	D:\workspace\crvci\crvci-web\src\main\java\com\crv\ci\action /e
-xcopy .\output\view	D:\workspace\crvci\crvci-web\src\main\webapp\WEB-INF\freemarker\views\module /e
+set dir=d:\workspace\crvci
+xcopy .\output\model	%dir%\crvci-domain\src\main\java\com\crv\ci\model /e
+xcopy .\output\dao	    %dir%\crvci-biz\src\main\java\com\crv\ci\dao /e
+xcopy .\output\service	%dir%\crvci-biz\src\main\java\com\crv\ci\service /e
+xcopy .\output\mybatis	%dir%\crvci-biz\src\main\resources\mybatis /e
+xcopy .\output\action	%dir%\crvci-web\src\main\java\com\crv\ci\action /e
+xcopy .\output\view	    %dir%\crvci-web\src\main\webapp\WEB-INF\freemarker\views\module /e
 PAUSE
